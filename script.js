c1c39dba1b20fdc83e858c23cd20b3e419a1c9cb
Mark Klingelhoefer <mkling5577@gmail.com>
2023-10-24 15:53:03 -0400

    Add mousedown event listener to each box
    Color of box can now change when the user is holding down the mouse
    button and hovering over a box

--- script.js
+++ script.js
@@ -10,15 +10,22 @@ for (let j = 0; j < (slider.value * slider.value); j++) {
     let box = document.createElement("div");
     box.classList.add("box");
     container.appendChild(box);
+    // Changes color of box whenever box is clicked
     box.addEventListener("click", function(e) {
         e.target.style.backgroundColor = "black";
+    });
+    // Changes color of box whenever mouse button is hovering over the box and mouse button is also held down
+    box.addEventListener("mouseover", function(e) {
+        if (e.buttons === 1) {
+            e.target.style.backgroundColor = "black";
+        }
     });
 };
 
 // Sets sliderOutput equal to slider input value
 sliderOutput.textContent = `${slider.value} x ${slider.value}`;
 
-// Changes slider output when bar is moved and adjusts grid to slider value
+// Changes slider output when bar is moved, adjusts grid to slider value, and adds event listeners to each box in the grid
 slider.oninput = function() {
     container.innerHTML = "";
     sliderOutput.textContent = `${slider.value} x ${slider.value}`;
@@ -27,13 +34,15 @@ slider.oninput = function() {
         let box = document.createElement("div");
         box.classList.add("box");
         container.appendChild(box);
+        // Changes color of box whenever box is clicked
         box.addEventListener("click", function(e) {
             e.target.style.backgroundColor = "black";
         });
+        // Changes color of box whenever mouse button is hovering over the box and mouse button is also held down
+        box.addEventListener("mouseover", function(e) {
+            if (e.buttons === 1) {
+                e.target.style.backgroundColor = "black";
+            }
+        });
     };
 }
-
-// Adds event listeners to each box
-const addListenerToBox = (e) => {
-    console.log(e);
-};
