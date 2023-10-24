e28e6e389ac9ed58d10247b66fbf6a4555f13efa
Mark Klingelhoefer <mkling5577@gmail.com>
2023-10-23 22:19:58 -0400

    Add event listeners to boxes
    Event listeners added to each box on the grid that change the background color of the box to black when clicked

--- script.js
+++ script.js
@@ -5,20 +5,35 @@ const sliderOutput = document.querySelector("#sliderOutput");
 // Sets default grid template
 container.style.gridTemplate = "repeat(4, 1fr) / repeat(4, 1fr)";
 
+// Sets default boxes
+for (let j = 0; j < (slider.value * slider.value); j++) {
+    let box = document.createElement("div");
+    box.classList.add("box");
+    container.appendChild(box);
+    box.addEventListener("click", function(e) {
+        e.target.style.backgroundColor = "black";
+    });
+};
+
 // Sets sliderOutput equal to slider input value
 sliderOutput.textContent = `${slider.value} x ${slider.value}`;
 
 // Changes slider output when bar is moved and adjusts grid to slider value
 slider.oninput = function() {
-    console.log(slider.value);
     container.innerHTML = "";
     sliderOutput.textContent = `${slider.value} x ${slider.value}`;
     container.style.gridTemplate = `repeat(${slider.value}, 1fr) / repeat(${slider.value}, 1fr)`;
-    console.log(container.style.gridTemplate);
-    console.log(container)
     for (let j = 0; j < (slider.value * slider.value); j++) {
         let box = document.createElement("div");
         box.classList.add("box");
         container.appendChild(box);
+        box.addEventListener("click", function(e) {
+            e.target.style.backgroundColor = "black";
+        });
     };
 }
+
+// Adds event listeners to each box
+const addListenerToBox = (e) => {
+    console.log(e);
+};
